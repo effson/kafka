590472d67504936115089e613f132f42453b5156
effson <113086278+effson@users.noreply.github.com>
2025-07-25 22:29:52 +0800

    Update kafkaConsumer.cpp

--- c/kafkaConsumer.cpp
+++ c/kafkaConsumer.cpp
@@ -4,7 +4,7 @@ KafkaConsumer::KafkaConsumer(const std::string &brokers,
                              const std::string &groupID,
                              const std::vector<std::string> &topics,
                              int partition){
-    m_brokers = broker;
+    m_brokers = brokers;
     m_groupID = groupID;
     m_topicvector = topics;
     m_partition = partition;
@@ -23,12 +23,7 @@ KafkaConsumer::KafkaConsumer(const std::string &brokers,
     errCode = m_config->set("rebalance_cb", m_rebalance_cb, errStr);
     if (errCode != RdKafka::Conf::CONF_OK) {
         std::cout << "Fail to set conf.rebalance_cb" << errStr << std::endl;
-    }
-                               
-    errCode = m_config->set("enable_partition.eof", "false", errStr); 
-    if (errCode != RdKafka::Conf::CONF_OK) {
-        std::cout << "Fail to set conf.enable_partition.eof" << errStr << std::endl;
-    }    
+    } 
 
     errCode = m_config->set("group.id", m_groupID, errStr); 
     if (errCode != RdKafka::Conf::CONF_OK) {
@@ -51,36 +46,50 @@ KafkaConsumer::KafkaConsumer(const std::string &brokers,
     } 
 
     m_topicConfig = RdKafka::Conf::create(RdKafka::Conf::CONF_TOPIC);
+    
+    errCode = m_topicConfig->set("auto.offset.reset", "latest", errStr);
+    if (errCode != RdKafka::Conf::CONF_OK) {
+        std::cout << "Fail to set topicConfig.auto.offset.reset" << errStr << std::endl;
+    }
+
+    errCode = m_config->set("default_topic_conf", m_topicConfig, errStr);
+    if (errCode != RdKafka::Conf::CONF_OK) {
+        std::cout << "Fail to set topicConfig.default_topic_conf" << errStr << std::endl;
+    }
+
+    m_consumer = RdKafka::KafkaConsumer::create(m_config, errStr);
     if (m_consumer == NULL) {
-        std::cout << "Create consumer " << m_consumer->name() << std::endl;
-    }                               
+        std::cout << "Fail to create Kafkaconsumer: " << errStr << std::endl;
+    }
+
+    std::cout << "Create KafkaConsumer " << m_consumer->name() << std::endl;
 }
 
 void msg_consume(RdKafka::Message *msg, void *opaque) {
     switch(msg->err()) {
-      case RdKafka::ERR__TIMED_OUT:
-          std::cerr << "Consumer error: " << msg->errstr() << std::endl;
-          break;
-      case RdKafka::ERR_NO_ERROR:
-          std::cout << "Message in->topic: " << msg->topic_name()
-                    << ", partition:[" << msg->partition() << "] at offset "
-                    << msg->offset() << "key: " << msg->key()
-                    << "payload: " << (char *)msg->payload() << std::endl;
-          break;
-      default:
-          std::cerr << "Consumer error: " << msg->errstr() << std::endl;
-          break;
+    case RdKafka::ERR__TIMED_OUT:
+        // std::cerr << "Consumer error: " << msg->errstr() << std::endl;
+        break;
+    case RdKafka::ERR_NO_ERROR:
+        std::cout << "Message in->topic: " << msg->topic_name()
+                  << ", partition:[" << msg->partition() << "] at offset "
+                  << msg->offset() << "key: " << msg->key()
+                  << "payload: " << (char *)msg->payload() << std::endl;
+        break;
+    default:
+        std::cerr << "Consumer error: " << msg->errstr() << std::endl;
+        break;
     }
 }
 
 void KafkaConsumer::pullMessage() {
-    RdKafka::ErrorCode errCode = m_consumer->subscribe(m_topicVector);
-    if (errCode != RdKafka::ERROR_NO_ERROR) {
-        std::cout << "Subscribe failed: " << RdKafka::err2str(errorCode)
+    RdKafka::ErrorCode errCode = m_consumer->subscribe(m_topicvector);
+    if (errCode != RdKafka::ERR_NO_ERROR) {
+        std::cout << "Subscribe failed: " << RdKafka::err2str(errCode)
                   << std::endl;
     }
     while (true) {
-        RdKafka::Message * msg = m_consumer(1000);
+        RdKafka::Message * msg = m_consumer->consume(1000);
         msg_consume(msg, NULL);
         delete msg;
     }
