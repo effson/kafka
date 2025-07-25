c2605ff145c634d006d78eab038b9e8370b09d2f
effson <113086278+effson@users.noreply.github.com>
2025-07-25 14:17:57 +0800

    Update kafkaConsumer.cpp

--- c/kafkaConsumer.cpp
+++ c/kafkaConsumer.cpp
@@ -9,9 +9,20 @@ KafkaConsumer::KafkaConsumer(const std::string &brokers
     m_topicvector = topics;
     m_partition = partition;
                                
-    std::string errorStr;
+    std::string errStr;
     RdKafka::Conf::ConfResult errCode;
     m_config = RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL);
-    
+
+    m_event_cb = new ConsumerEventCb;
+    errCode = m_config->set("event_cb".m_event_cb, errStr);
+    if (errCode != RdKafka::Conf::CONF_OK) {
+      std::cout << "Fail to set conf.event_cb" << errStr << std::endl;
+    }
+                               
+    m_rebalance_cb = new ConsumerRebalanceCb;
+    errCode = m_config->set("rebalance_cb".m_rebalance_cb, errStr);
+    if (errCode != RdKafka::Conf::CONF_OK) {
+      std::cout << "Fail to set conf.rebalance_cb" << errStr << std::endl;
+    }
                                
 }
